c33ca016c79fb78f48c4f86e9579b9353dad00b9
mremond <michaelremond@gmail.com>
2014-04-03 17:09:38 +0200

    add SAML to demo

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,40 +13,42 @@ import play.mvc.Content;
 import play.mvc.Result;
 
 public class Application extends JavaController {
-    
+
     public static Result index() throws TechnicalException {
         // profile (maybe null if not authenticated)
         final CommonProfile profile = getUserProfile();
-        final String urlFacebook = getRedirectionAction("FacebookClient", "/?0").getLocation();
-        final String urlTwitter = getRedirectionAction("TwitterClient", "/?1").getLocation();
-        final String urlForm = getRedirectionAction("FormClient", "/?2").getLocation();
-        final String urlBasicAuth = getRedirectionAction("BasicAuthClient", "/?3").getLocation();
-        final String urlCas = getRedirectionAction("CasClient", "/?4").getLocation();
-        final String urlGoogleOpenId = getRedirectionAction("GoogleOpenIdClient", "/?5").getLocation();
-        return ok(views.html.index.render(profile, urlFacebook, urlTwitter, urlForm, urlBasicAuth, urlCas, urlGoogleOpenId));
+        final String urlFacebook = getRedirectAction("FacebookClient", "/?0").getLocation();
+        final String urlTwitter = getRedirectAction("TwitterClient", "/?1").getLocation();
+        final String urlForm = getRedirectAction("FormClient", "/?2").getLocation();
+        final String urlBasicAuth = getRedirectAction("BasicAuthClient", "/?3").getLocation();
+        final String urlCas = getRedirectAction("CasClient", "/?4").getLocation();
+        final String urlGoogleOpenId = getRedirectAction("GoogleOpenIdClient", "/?5").getLocation();
+        final String urlSaml = getRedirectAction("Saml2Client", "/?6").getLocation();
+        return ok(views.html.index.render(profile, urlFacebook, urlTwitter, urlForm, urlBasicAuth, urlCas,
+                urlGoogleOpenId, urlSaml));
     }
-    
+
     private static Result protectedIndex() {
         // profile
         final CommonProfile profile = getUserProfile();
         return ok(views.html.protectedIndex.render(profile));
     }
-    
+
     @RequiresAuthentication(clientName = "FacebookClient")
     public static Result facebookIndex() {
         return protectedIndex();
     }
-    
+
     @RequiresAuthentication(clientName = "TwitterClient")
     public static Result twitterIndex() {
         return protectedIndex();
     }
-    
+
     @RequiresAuthentication(clientName = "FormClient")
     public static Result formIndex() {
         return protectedIndex();
     }
-    
+
     // Setting the isAjax parameter to true will result in a 401 error response
     // instead of redirecting to the login url.
     @RequiresAuthentication(clientName = "FormClient", isAjax = true)
@@ -56,12 +58,12 @@ public class Application extends JavaController {
         JsonContent jsonContent = new JsonContent(content.body());
         return ok(jsonContent);
     }
-    
+
     @RequiresAuthentication(clientName = "BasicAuthClient")
     public static Result basicauthIndex() {
         return protectedIndex();
     }
-    
+
     @RequiresAuthentication(clientName = "CasClient")
     public static Result casIndex() {
         /*final CommonProfile profile = getUserProfile();
@@ -74,12 +76,17 @@ public class Application extends JavaController {
         return ok(views.html.casProtectedIndex.render(profile, service, proxyTicket));*/
         return protectedIndex();
     }
-    
+
+    @RequiresAuthentication(clientName = "Saml2Client")
+    public static Result samlIndex() {
+        return protectedIndex();
+    }
+
     @RequiresAuthentication(clientName = "GoogleOpenIdClient")
     public static Result googleOpenIdIndex() {
         return protectedIndex();
     }
-    
+
     public static Result theForm() throws TechnicalException {
         final FormClient formClient = (FormClient) Config.getClients().findClient("FormClient");
         return ok(views.html.theForm.render(formClient.getCallbackUrl()));
