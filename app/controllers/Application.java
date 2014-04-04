cdc3aad067cbffcad31aab7275660cef15d0dd0e
mremond <michaelremond@gmail.com>
2014-04-04 18:02:34 +0200

    add saml support

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,39 +13,40 @@ import play.mvc.Content;
 import play.mvc.Result;
 
 public class Application extends JavaController {
-    
+
     public static Result index() throws TechnicalException {
         // profile (maybe null if not authenticated)
         final CommonProfile profile = getUserProfile();
-        final String urlFacebook = getRedirectionUrl("FacebookClient", "/?0");
-        final String urlTwitter = getRedirectionUrl("TwitterClient", "/?1");
-        final String urlForm = getRedirectionUrl("FormClient", "/?2");
-        final String urlBasicAuth = getRedirectionUrl("BasicAuthClient", "/?3");
-        final String urlCas = getRedirectionUrl("CasClient", "/?4");
-        return ok(views.html.index.render(profile, urlFacebook, urlTwitter, urlForm, urlBasicAuth, urlCas));
+        final String urlFacebook = getRedirectAction("FacebookClient", "/?0").getLocation();
+        final String urlTwitter = getRedirectAction("TwitterClient", "/?1").getLocation();
+        final String urlForm = getRedirectAction("FormClient", "/?2").getLocation();
+        final String urlBasicAuth = getRedirectAction("BasicAuthClient", "/?3").getLocation();
+        final String urlCas = getRedirectAction("CasClient", "/?4").getLocation();
+        final String urlSaml = getRedirectAction("Saml2Client", "/?4").getLocation();
+        return ok(views.html.index.render(profile, urlFacebook, urlTwitter, urlForm, urlBasicAuth, urlCas, urlSaml));
     }
-    
+
     private static Result protectedIndex() {
         // profile
         final CommonProfile profile = getUserProfile();
         return ok(views.html.protectedIndex.render(profile));
     }
-    
+
     @RequiresAuthentication(clientName = "FacebookClient")
     public static Result facebookIndex() {
         return protectedIndex();
     }
-    
+
     @RequiresAuthentication(clientName = "TwitterClient")
     public static Result twitterIndex() {
         return protectedIndex();
     }
-    
+
     @RequiresAuthentication(clientName = "FormClient")
     public static Result formIndex() {
         return protectedIndex();
     }
-    
+
     // Setting the isAjax parameter to true will result in a 401 error response
     // instead of redirecting to the login url.
     @RequiresAuthentication(clientName = "FormClient", isAjax = true)
@@ -55,12 +56,17 @@ public class Application extends JavaController {
         JsonContent jsonContent = new JsonContent(content.body());
         return ok(jsonContent);
     }
-    
+
     @RequiresAuthentication(clientName = "BasicAuthClient")
     public static Result basicauthIndex() {
         return protectedIndex();
     }
-    
+
+    @RequiresAuthentication(clientName = "Saml2Client")
+    public static Result samlIndex() {
+        return protectedIndex();
+    }
+
     @RequiresAuthentication(clientName = "CasClient")
     public static Result casIndex() {
         /*final CommonProfile profile = getUserProfile();
@@ -73,7 +79,7 @@ public class Application extends JavaController {
         return ok(views.html.casProtectedIndex.render(profile, service, proxyTicket));*/
         return protectedIndex();
     }
-    
+
     public static Result theForm() throws TechnicalException {
         final FormClient formClient = (FormClient) Config.getClients().findClient("FormClient");
         return ok(views.html.theForm.render(formClient.getCallbackUrl()));
