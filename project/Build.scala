e70d95775845d59fe120ec660b5eee862e1268d7
Jérôme LELEU <leleuj@gmail.com>
2014-04-08 17:39:11 +0200

    update libs

--- project/Build.scala
+++ project/Build.scala
@@ -8,12 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "org.pac4j" % "play-pac4j_java" % "1.1.2-SNAPSHOT",
-	    "org.pac4j" % "pac4j-http" % "1.5.0-SNAPSHOT",
-      "org.pac4j" % "pac4j-cas" % "1.5.0-SNAPSHOT",
-      "org.pac4j" % "pac4j-openid" % "1.5.0-SNAPSHOT",
-      "org.pac4j" % "pac4j-oauth" % "1.5.0-SNAPSHOT",
-      "org.pac4j" % "pac4j-saml" % "1.5.0-SNAPSHOT"
+      "org.pac4j" % "play-pac4j_java" % "1.1.2",
+	    "org.pac4j" % "pac4j-http" % "1.5.0",
+      "org.pac4j" % "pac4j-cas" % "1.5.0",
+      "org.pac4j" % "pac4j-openid" % "1.5.0",
+      "org.pac4j" % "pac4j-oauth" % "1.5.0",
+      "org.pac4j" % "pac4j-saml" % "1.5.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
