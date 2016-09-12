027fdcd41fa8fc5f796336adc58d021b4e68ce2a
leleuj <leleuj@gmail.com>
2016-09-12 18:33:28 +0200

    update to latest 1.9.2-SNAPSHOT

--- app/modules/SecurityModule.java
+++ app/modules/SecurityModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import controllers.CustomAuthorizer;
 import controllers.DemoHttpActionAdapter;
 import org.pac4j.cas.client.CasClient;
+import org.pac4j.cas.config.CasConfiguration;
 import org.pac4j.core.authorization.authorizer.RequireAnyRoleAuthorizer;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
@@ -16,6 +17,7 @@ import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
 import org.pac4j.oauth.client.FacebookClient;
 import org.pac4j.oauth.client.TwitterClient;
 import org.pac4j.oidc.client.OidcClient;
+import org.pac4j.oidc.config.OidcConfiguration;
 import org.pac4j.play.ApplicationLogoutController;
 import org.pac4j.play.CallbackController;
 import org.pac4j.play.cas.logout.PlayCacheLogoutHandler;
@@ -33,11 +35,9 @@ public class SecurityModule extends AbstractModule {
 
     public final static String JWT_SALT = "12345678901234567890123456789012";
 
-    private final Environment environment;
     private final Configuration configuration;
 
-    public SecurityModule(Environment environment, Configuration configuration) {
-        this.environment = environment;
+    public SecurityModule(final Environment environment, final Configuration configuration) {
         this.configuration = configuration;
     }
 
@@ -61,8 +61,9 @@ public class SecurityModule extends AbstractModule {
         // CAS
         // final CasOAuthWrapperClient casClient = new CasOAuthWrapperClient("this_is_the_key2", "this_is_the_secret2", "http://localhost:8080/cas2/oauth2.0");
         // casClient.setName("CasClient");
-        final CasClient casClient = new CasClient("https://casserverpac4j.herokuapp.com/login");
-        casClient.setLogoutHandler(new PlayCacheLogoutHandler(getProvider(CacheApi.class)));
+        final CasConfiguration casConfiguration = new CasConfiguration("https://casserverpac4j.herokuapp.com/login");
+        casConfiguration.setLogoutHandler(new PlayCacheLogoutHandler(getProvider(CacheApi.class)));
+        final CasClient casClient = new CasClient(casConfiguration);
 
         // casClient.setGateway(true);
         /*final CasProxyReceptor casProxyReceptor = new CasProxyReceptor();
@@ -78,11 +79,12 @@ public class SecurityModule extends AbstractModule {
         final SAML2Client saml2Client = new SAML2Client(cfg);
 
         // OpenID Connect
-        final OidcClient oidcClient = new OidcClient();
-        oidcClient.setClientID("343992089165-i1es0qvej18asl33mvlbeq750i3ko32k.apps.googleusercontent.com");
-        oidcClient.setSecret("unXK_RSCbCXLTic2JACTiAo9");
-        oidcClient.setDiscoveryURI("https://accounts.google.com/.well-known/openid-configuration");
-        oidcClient.addCustomParam("prompt", "consent");
+        final OidcConfiguration oidcConfiguration = new OidcConfiguration();
+        oidcConfiguration.setClientId("343992089165-i1es0qvej18asl33mvlbeq750i3ko32k.apps.googleusercontent.com");
+        oidcConfiguration.setSecret("unXK_RSCbCXLTic2JACTiAo9");
+        oidcConfiguration.setDiscoveryURI("https://accounts.google.com/.well-known/openid-configuration");
+        oidcConfiguration.addCustomParam("prompt", "consent");
+        final OidcClient oidcClient = new OidcClient(oidcConfiguration);
         oidcClient.addAuthorizationGenerator(profile -> profile.addRole("ROLE_ADMIN"));
 
         // REST authent with JWT for a token passed in the url as the token parameter
